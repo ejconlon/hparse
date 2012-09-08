dbf027cf313b325f6d484b9ca713e92e9b4e462e
Eric Conlon <ericatlooker@gmail.com>
2012-09-08 13:55:10 -0700

    Tokenize function

--- hparse.hs
+++ hparse.hs
@@ -9,10 +9,50 @@ import Test.HUnit
 
 main = hspec mySpecs
 
-square x = x * x
+tokenize :: String -> [String]
+tokenize s = reverse $ go s Nothing []
+  where
+        optAppendS :: Maybe String -> [String] -> [String]
+        optAppendS Nothing ts  = ts
+        optAppendS (Just t) ts = t:ts
+
+        optAppendC :: Char -> Maybe String -> String
+        optAppendC c Nothing = [c]
+        optAppendC c (Just t) = c:t
+
+        whitespace :: String
+        whitespace = " \t\r\n"
+
+        brackets :: String
+        brackets = "()"
+
+        go :: String -> Maybe String -> [String] -> [String] 
+        -- args:
+        --   remaining characters
+        --   current token
+        --   current emitted tokens (NB will be reversed!)
+        -- returns:
+        --   emitted tokens (NB will be reversed!)
+        go (c:s) mt ts | elem c whitespace = go s Nothing (optAppendS mt ts)
+                       | elem c brackets = go s Nothing ([c]:(optAppendS mt ts))
+                       | otherwise =  go s (Just $ optAppendC c mt) ts
+        go [] mt ts = optAppendS mt ts
+
+type TokenizeCase = (String, [String])
+
+tcaseList :: [TokenizeCase]
+tcaseList = [ ("()", ["(", ")"])
+            , ("(a b c)", ["(", "a", "b", "c", ")"])
+            , ("(a \tb)", ["(", "a", "b", ")"])
+            , ("( )(\n) ()", ["(", ")", "(", ")", "(", ")"])
+            ]
+
+tcaseRunner :: TokenizeCase -> Bool
+tcaseRunner (c, rs) = tokenize c == rs
+
+tcaseListRunner :: [TokenizeCase] -> Bool
+tcaseListRunner tcl = all id $ map tcaseRunner tcl
 
 mySpecs = describe "stuff" $ do
-  it "stuff2" $
-     square 2 == 4
-  it "stuff3" $
-     pending "stuff3 not done"
+  it "tokenizes" $
+     tcaseListRunner tcaseList
