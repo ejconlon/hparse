6aa1646f1eb69e81c35e649437f7f4828b46388b
Eric Conlon <ejconlon@gmail.com>
2012-09-09 13:47:23 -0700

    Parsing

--- hparse.hs
+++ hparse.hs
@@ -2,6 +2,7 @@
 
 {-# LANGUAGE MultiParamTypeClasses, ExistentialQuantification #-}
 
+import Debug.Trace
 import Test.Hspec.Monadic
 import Test.Hspec.QuickCheck
 import Test.Hspec.HUnit ()
@@ -9,6 +10,10 @@ import Test.QuickCheck
 import Test.HUnit
 
 main = hspec mySpecs
+
+traceEq :: forall a. (Show a, Eq a) => a -> a -> Bool
+traceEq x y | x == y = True
+            | otherwise = trace ("T1. " ++ (show x)) $ trace ("T2. " ++ (show y)) $ False
 
 class Case a where
   runCase :: a -> Bool
@@ -20,7 +25,7 @@ tokenize s = reverse $ go s Nothing []
   where
         optAppendS :: Maybe String -> [String] -> [String]
         optAppendS Nothing ts  = ts
-        optAppendS (Just t) ts = t:ts
+        optAppendS (Just t) ts = (reverse t):ts
 
         optAppendC :: Char -> Maybe String -> String
         optAppendC c Nothing = [c]
@@ -47,11 +52,12 @@ tokenize s = reverse $ go s Nothing []
 data TokenizeCase = TokenizeCase (String, [String])
 
 instance Case (TokenizeCase) where
-  runCase (TokenizeCase (c, rs)) = (tokenize c) == rs
+  runCase (TokenizeCase (c, rs)) = traceEq (tokenize c) rs
 
 tokenizeCases :: [TokenizeCase]
 tokenizeCases = map TokenizeCase [
               ("()", ["(", ")"])
+            , ("(abc)", ["(", "abc", ")"])
             , ("(a b c)", ["(", "a", "b", "c", ")"])
             , ("(a \tb)", ["(", "a", "b", ")"])
             , ("( )(\n) ()", ["(", ")", "(", ")", "(", ")"])
@@ -69,7 +75,7 @@ isBalanced (t:ts) = (t == "(") && (go 1 ts)
 data IsBalancedCase = IsBalancedCase ([String], Bool)
 
 instance Case (IsBalancedCase) where
-  runCase (IsBalancedCase (ts, r)) = (isBalanced ts) == r
+  runCase (IsBalancedCase (ts, r)) = traceEq (isBalanced ts) r
 
 appFirst :: forall x y b. (x -> y) -> (x, b) -> (y, b)
 appFirst f (a, b) = (f a, b)
@@ -87,45 +93,54 @@ isBalancedCases = map (IsBalancedCase . appFirst tokenize) [
 
 data Tree a = Leaf a | Branch [Tree a] deriving (Show, Eq)
 
-allJust :: forall a. [Maybe a] -> Maybe [a]
-allJust xs = go xs []
-  where
-    go :: [Maybe a] -> [a] -> Maybe [a]
-    go [] [] = Nothing  -- no Just [] in the end
-    go [] as = Just as
-    go (Nothing:xs) _ = Nothing
-    go ((Just a):xs) as = go xs (a:as)
-
-unrecurse :: forall a. Tree (Maybe a) -> Maybe (Tree a)
-unrecurse (Leaf Nothing) = Nothing
-unrecurse (Leaf (Just y)) = Just $ Leaf y
-unrecurse (Branch xs) = treeified
-  where
-    applied :: [Maybe (Tree a)]
-    applied = map unrecurse xs
-   
-    allJustApplied :: Maybe [Tree a]
-    allJustApplied = allJust applied
-
-    treeified :: Maybe (Tree a)
-    treeified = case allJustApplied of
-      Nothing -> Nothing
-      Just xs -> Just $ Branch xs 
+-- allJust :: forall a. [Maybe a] -> Maybe [a]
+-- allJust xs = go xs []
+--   where
+--     go :: [Maybe a] -> [a] -> Maybe [a]
+--     go [] [] = Nothing  -- no Just [] in the end
+--     go [] as = Just as
+--     go (Nothing:xs) _ = Nothing
+--     go ((Just a):xs) as = go xs (a:as)
+-- 
+-- unMaybeTree :: forall a. Tree (Maybe a) -> Maybe (Tree a)
+-- unMaybeTree (Leaf Nothing) = Nothing
+-- unMaybeTree (Leaf (Just y)) = Just $ Leaf y
+-- unMaybeTree (Branch xs) = case allJust (map unMaybeTree xs) of
+--       Nothing -> Nothing
+--       Just xs -> Just $ Branch xs 
 
 parse :: [String] -> Maybe (Tree String)
-parse ts = go ts
+parse ts = case go ts 0 [] of
+    Just ([], [mt]) -> Just mt
+    _ -> Nothing
   where
-    go :: [String] -> Maybe (Tree String)
-    go _ = Nothing
-
+    go :: [String] -> Int -> [Tree String] -> Maybe ([String], [Tree String])
+    go [] 0 [] = trace "HERE1" Nothing  -- empty branch
+    go [] 0 mts = Just ([], mts)
+    go [] _ _ = trace "HERE2" Nothing
+    go (")":ys) 0 _ = Nothing  -- close w/o open
+    go (")":ys) _ [] = Nothing  -- empty branch
+    go (")":ys) i mts = Just (ys, mts)
+    go ("(":ys) i mts = case go ys (i+1) [] of
+      Just (xs, mts2) -> go xs i ((Branch $ reverse $ mts2):mts)
+      Nothing -> Nothing
+    go (y:ys) i mts = go ys i ((Leaf y):mts)
 
 data ParseCase = ParseCase ([String], Maybe (Tree String))
 instance Case (ParseCase) where
-  runCase (ParseCase (ts, mb)) = (parse ts) == mb
+  runCase (ParseCase (ts, mb)) = traceEq (parse ts) mb
 
 parseCases :: [ParseCase]
 parseCases = map (ParseCase . appFirst tokenize) [
             ("(a)", Just $ Branch [Leaf "a"])
+          , ("(abc)", Just $ Branch [Leaf "abc"])
+          , ("((a))", Just $ Branch [Branch [Leaf "a"]])
+          , ("((a) b)", Just $ Branch [Branch [Leaf "a"], Leaf "b"])
+          , ("(a (b))", Just $ Branch [Leaf "a", Branch [Leaf "b"]])
+          , ("(a b c)", Just $ Branch [Leaf "a", Leaf "b", Leaf "c"])
+          , ("(a (b c))", Just $ Branch [Leaf "a", Branch [Leaf "b", Leaf "c"]])
+          , ("((a b) (c d))", Just $ Branch [Branch [Leaf "a", Leaf "b"], Branch [Leaf "c", Leaf "d"]])
+          , ("((a (b c)) ((d e) f))", Just $ Branch [Branch [Leaf "a", Branch [Leaf "b", Leaf "c"]], Branch [Branch [Leaf "d", Leaf "e"], Leaf "f"]])
           ]
 
 mySpecs = describe "stuff" $ do
